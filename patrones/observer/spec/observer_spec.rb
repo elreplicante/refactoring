7b8339ca4f1b0d4957acb8447fc5b41147af9927
Sergio Revilla <replymusic@gmail.com>
2014-10-16 13:52:27 +0200

    spec refactor

--- patrones/observer/spec/observer_spec.rb
+++ patrones/observer/spec/observer_spec.rb
@@ -2,19 +2,18 @@ require 'observer'
 require 'subject'
 
 describe Observer do
-  it "registers to a subject" do
-    observer = Observer.new
-    subject = Subject.new
 
+  let(:observer) { Observer.new }
+  let(:subject) { Subject.new }
+
+  it "registers to a subject" do
     subject.register observer
 
     expect(subject.observers).to eq 1
   end
 
   it "unregisters from a subject" do
-    observer = Observer.new
     other_observer = Observer.new
-    subject = Subject.new
 
     subject.register observer
     subject.register other_observer
